91c3cad3884797c5224c8417a71e2e73a5e40feb
t-ueno <t-ueno@bstinc.co.jp>
2024-12-17 14:09:41 +0900

    文末の句点を追加
    Laravel Fincode関連ファイルの説明文に文末句点を追加しました。これにより、説明文に一貫したフォーマットが適用され、可読性が向上します。

--- src/Console/Commands/PublishCommand.php
+++ src/Console/Commands/PublishCommand.php
@@ -5,12 +5,12 @@ namespace Fincode\Laravel\Console\Commands;
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Attribute\AsCommand;
 
-#[AsCommand(name: 'fincode:publish', description: 'Publish the Laravel Fincode files')]
+#[AsCommand(name: 'fincode:publish', description: 'Publish the Laravel Fincode files.')]
 class PublishCommand extends Command
 {
     protected $signature = 'fincode:publish';
 
-    protected $description = 'Publish the Laravel Fincode files';
+    protected $description = 'Publish the Laravel Fincode files.';
 
     public function handle(): void
     {
