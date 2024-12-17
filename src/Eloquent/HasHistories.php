de10946ac644f7c1ccc279e7dab59443465243d0
t-ueno <t-ueno@bstinc.co.jp>
2024-12-17 19:50:09 +0900

    リレーションメソッドとクラスのインポートを追加
    変更履歴とのリレーション用メソッド `histories` を追加し、関連するモデルクラス `FinHistory` をインポートしました。また、変更履歴処理のフックコメントも補足として実装に追加しました。

--- src/Eloquent/HasHistories.php
+++ src/Eloquent/HasHistories.php
@@ -2,8 +2,10 @@
 
 namespace Fincode\Laravel\Eloquent;
 
+use Fincode\Laravel\Models\FinHistory;
 use Fincode\Laravel\Observers\HasHistoriesObserver;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 /**
  * @mixin Model
@@ -12,6 +14,15 @@ trait HasHistories
 {
     public static function bootHasHistories(): void
     {
+        // 変更履歴処理をフックする
         static::observe(HasHistoriesObserver::class);
+    }
+
+    /**
+     * 変更履歴とのリレーション
+     */
+    public function histories(): MorphMany|FinHistory
+    {
+        return $this->morphMany(FinHistory::class, 'source')->withTrashed();
     }
 }
