cb07f6c3ee198cbaa60dfc66abb32d368a13f5be
t-ueno <t-ueno@bstinc.co.jp>
2024-12-23 13:44:26 +0900

    新しい定数をFinHistoryType列挙型に追加
    FinHistoryType列挙型にRESTOREとFORCE_DELETEの定数を追加しました。これにより、復元操作と物理削除操作を識別可能になりました。

--- src/Enums/FinHistoryType.php
+++ src/Enums/FinHistoryType.php
@@ -4,9 +4,18 @@ namespace Fincode\Laravel\Enums;
 
 enum FinHistoryType: int
 {
+    // 新規登録
     case INSERT = 1;
+
+    // 更新
     case UPDATE = 2;
+
+    // 論理削除
     case DELETE = 3;
+
+    // 復元
     case RESTORE = 4;
+
+    // 物理削除
     case FORCE_DELETE = 5;
 }
