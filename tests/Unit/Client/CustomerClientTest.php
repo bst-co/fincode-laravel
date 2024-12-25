fbbdad4ae19eed67547da2b9c33de83050b61abc
t-ueno <t-ueno@bstinc.co.jp>
2024-12-25 20:35:24 +0900

    新しいテストケースを追加し既存のテストを改善
    `CustomerClientTest`に新規作成と削除のテストを追加し、`CustomerCommandTest`では詳細ログのオプションを反映しました。これにより、テストカバレッジが向上しデバッグが容易になります。

--- tests/Unit/Client/CustomerClientTest.php
+++ tests/Unit/Client/CustomerClientTest.php
@@ -8,6 +8,28 @@ use Fincode\Laravel\Test\TestCase;
 
 class CustomerClientTest extends TestCase
 {
+    public function test_create()
+    {
+        $client = new FincodeCustomerRequest;
+
+        $customer = FinCustomer::factory()->make();
+
+        dump($customer->toArray());
+
+        // Testing Create
+        $model = tap($client->create($customer))->save();
+
+        $this->assertInstanceOf(FinCustomer::class, $model);
+        $this->assertTrue($model->exists);
+
+        $model = tap($client->delete($model->id))->delete();
+
+        $this->assertInstanceOf(FinCustomer::class, $model);
+        $this->assertTrue($model->trashed());
+
+        dump($model->toArray());
+    }
+
     public function test_retrieve()
     {
         dump(env('FINCODE_API_KEY'));
--- tests/Unit/CustomerCommandTest.php
+++ tests/Unit/CustomerCommandTest.php
@@ -6,12 +6,20 @@ use Fincode\Laravel\Test\TestCase;
 
 class CustomerCommandTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
     public function test_token() {}
 
     public function test_customer_retrieve()
     {
         $result = $this->artisan('fincode:customer:retrieve', [
             'customer_id' => 'fin_test_1111',
-        ])->assertOk();
+            '-vvv' => true,
+        ])
+            ->assertOk()
+            ->run();
     }
 }
